ea0e88724d16023303bab2eec56dfcdd8480f37e
Huy Dang <hqdang97@gmail.com>
2018-12-18 20:26:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path')
+const port = 8080;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use( express.static( `${__dirname}/build` ) );
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'build','index.html'));
+})
+
+app.listen(port, () => {
+    console.log('server is listening on port ', port)
+})
